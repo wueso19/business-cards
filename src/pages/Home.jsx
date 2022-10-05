b46e499c656098655025d2149c0dd48011515330
Brian <wueso19@admin.com>
2022-10-05 02:00:18 -0300

    Initial Template

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-center text-5xl font-bold mt-4">
+        TARJETAS DE PRESENTACIÓN
+      </h1>
+    </>
+  );
+};
+export default Home;
